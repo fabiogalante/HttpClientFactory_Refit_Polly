438cd146506df6bacb84e01fc8bc3595232f8a8e
Fabio Galantes Mans <fmans.3mpk@sofisa.com.br>
2020-06-09 19:53:43 -0300

    settings

--- HttpClientFactoryProject/Models/ClienteSofisaDireto.cs
+++ HttpClientFactoryProject/Models/ClienteSofisaDireto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HttpClientFactoryProject.Models
+﻿namespace HttpClientFactoryProject.Models
 {
     public class ClienteSofisaDireto
     {
